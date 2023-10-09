d2f43f90c9fdecbd75608a71368f1bc1f568b922
Shubham Soni <111747217+Shubham-5547@users.noreply.github.com>
2023-10-09 23:13:30 +0530

    Construction Of BST

--- src/dsa/BST/002_Bst Construction.js
+++ src/dsa/BST/002_Bst Construction.js
@@ -11,3 +11,90 @@
 // Each BST node has an integer value, a left child node, and a right child node. A node is said to be a valid BST node if and only if it satisfies the BST property: 
 // its value is strictly greater than the values of every node to its left; its value is less than or equal to the values of every node to its right; 
 // and its children nodes are either valid BST nodes themselves or None / null.
+
+
+// Average time O(logn) | space O(1)
+// Worst time O(n) | space O(1)
+
+class BST {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(value) {
+    if ( value < this.value) {
+      if (this.left === null) {
+        this.left = new BST(value);
+      } else {
+        this.left.insert(value);
+      }
+    } else {
+      if (this.right === null) {
+        this.right = new BST(value);
+      } else {
+        this.right.insert(value);
+      }
+    }
+    return this;
+  }
+
+  contains(value) {
+    if ( value < this.value) {
+      if (this.left === null) {
+        return false;
+      } else {
+        return this.left.contains(value);
+      }
+    } else if ( value > this.value) {
+      if (this.right === null) {
+        return false;
+      } else {
+        return this.right.contains(value);
+      }
+    } else {
+      return true;
+    }
+  }
+
+  remove(value, parent = null) {
+    if ( value < this.value) {
+      if (this.left !== null) {
+        this.left.remove(value, this);
+      }
+    } else if (value > this.value) {
+      if ( this.right !== null) {
+        this.right.remove(value, this);
+      }
+    } else {
+      if ( this.left !== null && this.right !== null) {
+        this.value = this.right.getMinValue();
+        this.right.remove(this.value, this);
+      } else if ( parent === null) {
+      if (this.left !== null) {
+        this.value = this.left.value;
+        this.right = this.left.right;
+        this.left = this.left.left;
+      } else if (this.right !== null) {
+        this.value = this.right.value;
+        this.left = this.right.left;
+        this.right = this.right.right;
+      } else {
+      }
+      } else if (parent.left === this) {
+        parent.left = this.left !== null ? this.left : this.right;
+      } else if (parent.right === this) {
+        parent.right = this.right !== null ? this.right : this.left;
+      }
+    }
+    return this;
+  }
+  getMinValue() {
+    if(this.left === null) {
+      return this.value;
+    } else {
+      return this.left.getMinValue();
+    }
+  }
+}
